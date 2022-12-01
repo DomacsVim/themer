dfd9c5a97ab0548c4c39a328a787fff5c8c92b29
Sameet <sameetmoosavi@yahoo.com>
2022-12-01 18:21:45 +0330

    chore(nightfox) remove nightfox colorscheme

--- colors/dayfox.lua
+++
@@ -1,6 +0,0 @@
-if vim.g.nightfox_debug then
-  require("themer.themes.nightfox.util.reload")()
-end
-
-require("themer.themes.nightfox.config").set_fox("dayfox")
-require("themer.themes.nightfox").load()
--- lua/themer/themes/nightfox/fingerprint.lua
+++
@@ -1,1 +0,0 @@
-return [[9c53560edd689ddeda1adb0666b3489e2d89aec3]]
